94cec479ce5de860ab1a0a6386ef75c4f2f6a74f
Jasper Kloster <jasper.kloster@student.tsc.nsw.edu.au>
2018-05-22 10:18:23 +1000

    Reworked Project
    Implemented a tilemap, Shoot system works

---
+++ Bruvforce/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+
+	public float velocityX = 100f;
+	float velocityY = 0f;
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		rb.velocity = new Vector2(velocityX, velocityY);
+
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag != "Player")
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Bruvforce/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 5.0f;
+	public float jumpheight;
+	public bool canjump = true;
+
+
+	public Transform bulletspawn;
+	public GameObject bullet;
+	GameObject tempbullet;
+
+	public CircleCollider2D groundCheck;
+
+	Rigidbody2D rigid;
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = this.GetComponent<Animator> ();
+		rigid = this.GetComponent<Rigidbody2D> ();
+		canjump = true;
+
+		bulletspawn = this.gameObject.transform.GetChild(1);
+        
+	}
+
+	void FixedUpdate () {
+		if (Input.GetKey (KeyCode.D)) {
+			transform.position += Vector3.right * speed * Time.deltaTime;
+        } else if (Input.GetKey (KeyCode.A)) {
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+
+		if (Input.GetKey (KeyCode.Space) && canjump == true) {
+			rigid.velocity = new Vector2 (0, jumpheight);
+			canjump = false;
+		}
+        
+		if (Input.GetMouseButtonDown(0)){
+			tempbullet = Instantiate(bullet, bulletspawn.position, Quaternion.identity);
+			Destroy(tempbullet, 2f);
+		}
+	}
+
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		
+	    CheckIfGrounded ();
+	}
+
+	void CheckIfGrounded()
+	{
+		RaycastHit2D[] hits;
+
+		Vector2 positionToCheck = transform.position;
+		hits = Physics2D.RaycastAll(positionToCheck, new Vector2(0, -1), 0.01f);
+
+		canjump |= hits.Length > 0;
+	}
+}
